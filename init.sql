3dc725d307232018b32d7ee17e45f0d1610cb213
Jamie <james_wooltorton@hotmail.com>
2020-06-09 23:02:01 +0100

    Added source_types table, relationships and basic data

--- init.sql
+++ init.sql
@@ -1,15 +1,42 @@
-create table sources(
-	 				id SERIAL PRIMARY KEY,
+-- Create tables
+create table source_types(
+	 				source_type_id SERIAL PRIMARY KEY,
                     name varchar NOT NULL, 
-                    description text, 
-                    source_type varchar(50),
-                    endpoint varchar(200),
                     is_deleted bool default(false),
                     is_active bool default(true),
                     created timestamp without time zone default (now() at time zone 'utc')
                     );
 
 
+create table sources(
+	 				sourceId SERIAL PRIMARY KEY,
+                    name varchar NOT NULL, 
+                    description text, 
+                    source_type_id INT,
+                    endpoint varchar,
+                    is_deleted bool default(false),
+                    is_active bool default(true),
+                    created timestamp without time zone default (now() at time zone 'utc')
+                    );
+
+
+-- Add Foreign Keys
+
+ALTER TABLE sources
+    ADD CONSTRAINT fk_sources_source_types FOREIGN KEY (source_type_id) REFERENCES source_types (source_type_id);
+
+-- Insert source_types data
+
+INSERT INTO public.source_types( name) VALUES ('CSV');
+INSERT INTO public.source_types( name) VALUES ('SQL');
+INSERT INTO public.source_types( name) VALUES ('JSON');
+
+-- Insert sources data
+
 INSERT INTO public.sources(
-	name, source_uuid, description)
-	VALUES ('Test', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Blah blah blah')
+	name, description, source_type_id)
+	VALUES ('Postgres', 'Postgres connection', 2)
+
+INSERT INTO public.sources(
+	name, description, source_type_id)
+	VALUES ('Schema', 'Schema json file', 3)
